8e7ac72e61aa7ab4318360e9ea50bdd51426b8c9
chgl <5307555+chgl@users.noreply.github.com>
2026-07-03 20:31:25 +0200

    warn

--- src/main/java/io/github/dizuker/medrezeptetofhir/edqm/IfaDoseFormMapper.java
+++ src/main/java/io/github/dizuker/medrezeptetofhir/edqm/IfaDoseFormMapper.java
@@ -107,6 +107,6 @@ public final class IfaDoseFormMapper {
   public static Optional<Coding> lookup(String ifaCode) {
     return Optional.ofNullable(IFA_TO_EDQM.get(ifaCode))
         .flatMap(EdqmStandardterms::fromValue)
-        .map(EdqmStandardterms::coding);
+        .map(term -> term.coding());
   }
 }
